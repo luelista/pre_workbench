4a8f619cfa5c166be40f94d5962405d0033700b5
Mira Weller <mweller@seemoo.tu-darmstadt.de>
2022-07-04 14:26:14 +0200

    improve test cases: check round trip syntax

--- tests/parse_helper.py
+++ tests/parse_helper.py
@@ -1,3 +1,4 @@
+import inspect
 import os.path
 
 from binascii import unhexlify
@@ -8,6 +9,8 @@ log = False
 
 def parse_me(definition, hexstring, expected):
 	fic = FormatInfoContainer(load_from_string=definition)
+	definition_roundtrip = fic.to_text().replace("\t", " " * 8)
+	assert definition_roundtrip == inspect.cleandoc(definition)
 	pc = ParseContext(fic, unhexlify(hexstring.replace(" ","")), logging_enabled=log)
 	result = pc.parse()
 	if pc.failed:
